7d221cab4f93310538df4db3929a8c115c6c77f6
erdiiinn <erdinshala01@gmail.com>
2025-03-25 16:08:33 +0100

    Changes

---
+++ lesson4/lesson4.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    // phpinfo();
+
+    // $x="Hello";
+    // print_r($x);
+
+    // $x=5;
+    // echo gettype($x) . "<br>";
+    ?>
+    
+    <?php 
+    // function displayPhpVersion(){
+    //     echo "This is PHP" .phpversion();
+    //     echo "\n";
+    // }
+
+    // displayPhpversion();
+    ?>
+
+    <?php 
+
+    function hello(){
+        // echo "Hello World!";
+        // }
+        
+        // hello();
+    }
+        ?>
+
+    <?php 
+        
+    // function sum(){
+    //     $value = 120 + 20;
+    //     echo = $value;
+    // }
+
+    // sum();
+
+    ?>
+
+    <?php 
+
+    // function maximum($x , $y){
+    //     if($x > $y){
+    //         return $x;
+    //     }else
+    //     return $y;
+    // }
+
+    // $a = 20;
+    // $b = 30;
+
+    // $test = maximum($a , $b);
+    // echo "The max of $a and $b is $test";
+    ?>
+
+    <?php
+
+    function divisible();
+    $x = 2;
+    $y = 3;
+    $z
+
+    ?>
+</body>
+</html>
