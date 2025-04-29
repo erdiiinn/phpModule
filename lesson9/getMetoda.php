0ede76c65dc9578ebaccf0516e10eaf169a0b553
erdiiinn <erdinshala01@gmail.com>
2025-04-29 16:07:29 +0200

    Create getMetoda.php

---
+++ lesson9/getMetoda.php
@@ -0,0 +1,35 @@
+<?php 
+
+ $username = _GET [$username];
+ $password = _GET [$password];
+
+ echo $username;
+ echo "<br>"
+ echo $password;
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="getMetoda"></form>
+
+    <label for="username"> username </label>
+    <input type="text" name="username" id="username">
+    <label for="password"> passowrd </label>
+    <input type="password" name="password" id="password">
+
+    <input type="submit" value="submit">
+
+</body>
+</html>
