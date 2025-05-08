ea27a8e3e1c5079b5b3b8c0e6f0990e5ce6ca2a3
erdiiinn <erdinshala01@gmail.com>
2025-05-08 16:07:16 +0200

    Lesson10

---
+++ lesson10/dashboard.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?
+    include_once("config.php");
+    $sql = "SELECT * FROM users";
+    $getUsesrs = $conn -> prepare($sql);
+    $getUsers -> execute();
+
+    $users = $getUsers -> fetchAll();
+    
+    ?>
+
+    <table>
+        <thead>
+            <th>id</th>
+            <th>name</th>
+            <th>surname</th>
+            <th>email</th>
+        </thead>
+
+        <tbody>
+            <?php
+                foreach($users as $user){
+                    
+            ?>
+
+            <tr>
+                <td>
+                    <? $user['id'] ?>
+                </td>
+                <td>
+                    <? $user['name'] ?>
+                </td>
+
+                <td>
+                    <? $user['surname'] ?>
+                </td>
+
+                <td>
+                    <? $user['email'] ?>
+                </td>
+            </tr>
+
+            <?php 
+            }
+            ?>
+        </tbody>
+
+        <a href="add.php"></a>
+    </table>
+</body>
+</html>
