448681463580c2ae646d42a369f37792a11a6e3e
erdiiinn <erdinshala01@gmail.com>
2025-05-15 16:03:38 +0200

    Lesson12

---
+++ lesson12/footer.php
@@ -0,0 +1,9 @@
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxXep9tiCXs/Z9fNfEXiDAYTyjMaEBasjfuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+    integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYY1CeL727kskC66kF92t6XL2V" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ lesson12/signup.php
@@ -0,0 +1,29 @@
+<?php include("header.php"); ?>
+
+    <div class="signin">
+
+        <form action="register.php" method="post" class="form-signin" >
+             
+            <h1 class="h33 mb-3 font-weight-normal"> Please sign up</h1>
+
+            <label for="inputEmail" class="sr-only"> Name </label>
+            <input type="text" name="inputEmail" id="form-control" placeholder="name" name="name" required autofocus>
+
+            <label for="inputEmail" class="sr-only"> Surname </label>
+            <input type="text" name="inputEmail" id="form-control" placeholder="surname" name="surname" required autofocus>
+
+            <label for="inputEmail" class="sr-only"> Username </label>
+            <input type="text" name="inputEmail" id="form-control" placeholder="username" name="username" required autofocus>
+
+            <label for="inputEmail" class="sr-only"> Email </label>
+            <input type="email" name="inputEmail" id="form-control" placeholder="email" name="email" required autofocus>
+
+            <label for="inputEmail" class="sr-only"> Password </label>
+            <input type="password" name="inputEmail" id="form-control" placeholder="password" name="password" required autofocus>
+
+            <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit"> Sign Up </button>
+            
+        </form>
+    </div>
+
+    <?php include("footer.php"); ?>
